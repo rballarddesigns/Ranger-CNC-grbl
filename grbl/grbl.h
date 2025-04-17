19b7e366d939b655b2599b417bbbdcdd3a8a0145
rballarddesigns <ryan@rballarddesigns.com>
2025-04-17 09:22:46 -0400

    Update grbl.h
    #define GRBL_VERSION "1.1h Ranger CNC"

--- grbl/grbl.h
+++ grbl/grbl.h
@@ -22,7 +22,7 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.1h"
+#define GRBL_VERSION "1.1h RANGER CNC"
 #define GRBL_VERSION_BUILD "20190830"
 
 // Define standard libraries used by Grbl.
